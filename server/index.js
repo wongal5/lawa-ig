fc8e44b0e86bfa1896fdfec8b6bd562dfe8779ae
wongal5 <a.wong9970@gmail.com>
2018-01-08 17:04:19 -0800

    fixed server static serve path

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-app.use(express.static('client/dist'))
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/', (req, res) => res.sendStatus(200));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 //routes here
