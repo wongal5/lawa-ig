ca30e7ee70251d9482196543953ca4c4e3a61495
Aaron Pietsch <apietsch4@gmail.com>
2018-01-08 21:02:58 -0800

    add picture grid w/ semantic

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ComponentOne from './components/component1.jsx';
+import PictureGrid from './components/PictureGrid.jsx';
  
+//dummy photo data
+
+const photos = [
+  {url: 'https://media.alienwarearena.com/media/tux-r.jpg'},
+  {url: 'https://media.alienwarearena.com/media/tux-r.jpg'},
+  {url: 'https://media.alienwarearena.com/media/tux-r.jpg'},
+  {url: 'https://media.alienwarearena.com/media/tux-r.jpg'},
+  {url: 'https://media.alienwarearena.com/media/tux-r.jpg'},
+  {url: 'https://media.alienwarearena.com/media/tux-r.jpg'},
+  {url: 'https://media.alienwarearena.com/media/tux-r.jpg'},
+  {url: 'https://media.alienwarearena.com/media/tux-r.jpg'},
+  {url: 'https://media.alienwarearena.com/media/tux-r.jpg'}
+];
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: photos
+    };
+  }
   render() {
     return (
       <div>
         <h1>React is Live</h1>
-        <ComponentOne />
+        <PictureGrid allPhotos={this.state.photos} />
       </div>
-    )
+    );
   }
 }
  
