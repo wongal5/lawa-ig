ac8432f055643abb4c256fe7fba278f3bb294066
Your Name <williamputnamjr@gmail.com>
2018-01-10 17:08:22 -0800

    had to unrequire config in server

--- server/LogInServerModule.js
+++ server/LogInServerModule.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
-const config = require('./config.js');
+// const config = require('./config.js');
 
 // passport's sole purpose is to authenticate requests
 const passport = require('passport');
