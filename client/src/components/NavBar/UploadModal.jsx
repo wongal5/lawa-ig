bf6459a755738c4cdad3d18605c3bd4cd78bb1fe
wongal5 <a.wong9970@gmail.com>
2018-01-11 14:53:31 -0800

    rebase

--- client/src/components/NavBar/UploadModal.jsx
+++ client/src/components/NavBar/UploadModal.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Menu, Modal, Button, Form, Message } from 'semantic-ui-react';
 import Dropzone from 'react-dropzone';
+import axios from 'axios';
 
 class UploadModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      submitFlag: 'not submitted'
+      submitFlag: 'not submitted',
+      uploadedFile: null
     }
     this.insertForm = this.insertForm.bind(this);
   }
@@ -16,6 +18,17 @@ class UploadModal extends React.Component {
       this.setState({
         submitFlag: 'invalid file'
       })
+    } else {
+      this.setState({
+        submitFlag: 'not submitted',
+        uploadedFile: acceptedFiles
+      })
+      var image = new FormData();
+      image.append('image', acceptedFiles[0]);
+      axios.post('/upload', image)
+        .catch(err => {
+          console.log('image post failed', err);
+        })
     }
   }
 
@@ -29,6 +42,18 @@ class UploadModal extends React.Component {
     this.setState({
       submitFlag: 'not submitted'
     })
+  }
+
+  render() {
+    return (
+      <Modal className="upload-modal" size='small' onOpen={this.onOpen.bind(this)} trigger={<Menu.Item name='upload' icon='upload' />}>
+        <Dropzone accept="image/jpeg, image/png" maxSize={5000000} onDrop={this.onDrop.bind(this)}> 
+          <p>Try dropping some files here, or click to select files to upload.</p>
+          <p>Only *.jpeg and *.png images will be accepted</p>
+        </Dropzone>
+        {this.insertForm(this.state.submitFlag)}
+      </Modal>
+    );
   }
 
   insertForm(submitFlag) {
@@ -58,7 +83,7 @@ class UploadModal extends React.Component {
         <Message
           error
           header='Upload Error'
-          content="Uhoh! We don't accept that file type. Try again with a .jpg or .png."
+          content="Uhoh! We don't accept that file type or the file is too big. Please try again with a .jpg or .png under 5MB."
         />
         <Button>Submit</Button>
       </Form>)
@@ -73,18 +98,6 @@ class UploadModal extends React.Component {
         <Button>Submit</Button>
       </Form>)
     }
-  }
-
-  render() {
-    return (
-      <Modal className="upload-modal" size='large' onOpen={this.onOpen.bind(this)} trigger={<Menu.Item name='upload' icon='upload' />}>
-        <Dropzone accept="image/jpeg, image/png" onDrop={this.onDrop.bind(this)}> 
-          <p>Try dropping some files here, or click to select files to upload.</p>
-          <p>Only *.jpeg and *.png images will be accepted</p>
-        </Dropzone>
-        {this.insertForm(this.state.submitFlag)}
-      </Modal>
-    );
   }
 }
 
