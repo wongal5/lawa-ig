50c6c6f6b76818ede7fd28436ee96e58f5edad59
Larry <lchang2190@gmail.com>
2018-01-10 21:58:54 -0800

    feed progress

--- client/src/components/main_feed_pg/feed.jsx
+++ client/src/components/main_feed_pg/feed.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Container, Image, Feed, Icon, Segment, Divider, Button } from 'semantic-ui-react';
+import { Comment} from 'semantic-ui-react';
 import FeedGrid from './feed_grid.jsx';
 
 const OneFeed = (props) => (
 	<div className="ui card">
 	  <div className="content">
-	    <div className="right floated meta">14h</div>
 	    <img className="ui avatar image" src="./assets/fred.png"/>Fred
 	  </div>
 	  <div className="image">
 	    <img src={props.photo}/>
 	  </div>
 	  <div className="content">
-	    <span className="right floated">
+	    <span className="left floated">
 	      <i className="heart outline like icon"></i>
-	      17 likes
-	    </span>
-	    <i className="comment icon"></i>
-	    3 comments
+	      <i className="comment icon"></i>
+	    </span><br/>
+	    <div className="likes">17 likes</div><br/>
+	    <div className="comments">
+	  		<Comment.Text className='modal-author' as='a'>Albert</Comment.Text>
+      	<Comment.Text className='modal-comment'>How artistic!</Comment.Text><br />
+	  	</div>
+	    <div className="left floated meta">14h</div>
 	  </div>
 	  <div className="extra content">
 	    <div className="ui large transparent left icon input">
