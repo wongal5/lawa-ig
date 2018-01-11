53366e84a933e8866516d8a30df190671cf5d85b
wongal5 <a.wong9970@gmail.com>
2018-01-10 16:38:35 -0800

    test commit

--- test/databaseTests.js
+++ test/databaseTests.js
@@ -1,6 +1,6 @@
 // refer to jasmine docs here --> https://jasmine.github.io/api/2.8/global.html
 
-var database = require('../database/index.js');
+// var database = require('../database/index.js');
 
 // describe('TESTED FUNCTION NAME', function () {
 //   
