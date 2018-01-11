47c6f8dfe5b747991658baa2bb9ffee082daa47a
wongal5 <a.wong9970@gmail.com>
2018-01-10 21:46:47 -0800

    working feed requests

--- database/index.js
+++ database/index.js
@@ -7,56 +7,30 @@ const connection = {
 }
 // ADD PATH TO DATABASE URL ABOVE
 
-const client = new pg.Pool(connection);
-client.connect();
+const pool = new pg.Pool(connection);
+pool.connect();
 
 //get all users following
-const getUsersFollowing = function(userId, callback) {
-	return client.query(`SELECT followed_user FROM followers WHERE following_user = ${userId}`);
+const getUsersFollowing = function(userId) {
+	return pool.query('SELECT followed_user FROM followers WHERE following_user = $1', [userId]);
 }
 
 //get user followers
-const getUsersFollowers = function(userId, callback) {
-	var query = client.query("SELECT following_user FROM followers WHERE followed_user = 1");
-	query.then(res => {
-		console.log(res);
-	})
-		.catch(err=> {
-			console.log('error', err);
-		})
-	// client.query(`SELECT following_user FROM followers WHERE followed_user = $1`, [userId], function (error, results) {
-	// 	if (error) {
-	// 		console.log('select get user followers fail')
-	// 	} else {
-	// 		console.log('select get user followers success')
-	// 		callback(results);
-	// 	}
-	// })
+const getUsersFollowers = function(userId) {
+	return pool.query('SELECT following_user FROM followers WHERE followed_user = $1', [userId]);
 }
 
 //get all posts following
-const getAllPosts = function (userId, callback) {
-	client.query('SELECT posts.post_id, posts.img, posts.like_count, posts.user_id, posts.caption, posts.created_at FROM \
-		posts INNER JOIN followers ON followers.following_user = (?) AND \
-		followers.followed_user = posts.user_id', userId, function (error, results) {
-		if (error) {
-			console.log('select all posts fail')
-		} else {
-			console.log('select all posts success')
-			callback(results);
-		}
-	});
+const getAllPosts = function (userId) {
+	return pool.query('SELECT posts.post_id, posts.img, posts.like_count, posts.user_id, posts.caption, posts.created_at FROM \
+		posts INNER JOIN followers ON followers.following_user = $1 AND \
+		followers.followed_user = posts.user_id', [userId])
 }
 
-const getPostsLiked = function (userId, postsIdArray, callback) {
-	client.query('SELECT likes.post_id FROM likes INNER JOIN posts ON posts.post_id = likes.post_id AND likes.user_id = (?) WHERE likes.post_id IN (?)', userId, postsIdArray, function (error, results) {
-		if (error) {
-			console.log('get posts liked fail')
-		} else {
-			console.log('get posts liked success')
-			callback(results);
-		}
-	});
+const getPostsLiked = function (userId, postsIdArray) {
+	return pool.query('SELECT likes.post_id FROM likes INNER JOIN posts \
+	ON posts.post_id = likes.post_id AND likes.user_id = $1', 
+	[userId])
 }
 
 //get user following
--- server/index.js
+++ server/index.js
@@ -13,23 +13,43 @@ app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.get('/following', function(req, res) {
-  db.getUsersFollowing(1)
+app.get('/usersfollowing', function(req, res) {
+  db.getUsersFollowing(1) //change to req.body
     .then((results) => {
       res.json(results.rows.map(item => {return item.followed_user}));
     })
     .catch((err) => {
-      console.log('error!!!', err);
+      console.log('getUsersFollowing had an error', err);
     })
 })
 
-app.get('/followers', function (req, res) {
-  db.getUsersFollowing(1)
-    .then((result) => {
-      console.log('getUsersFollowing', );
+app.get('/usersfollowers', function (req, res) {
+  db.getUsersFollowers(1) //change to req.body
+    .then((results) => {
+      res.json(results.rows.map(item => { return item.following_user }));
     })
     .catch((err) => {
-      console.log('error!!!', err);
+      console.log('getUsersFollowers had an error', err);
+    })
+})
+
+app.get('/feed', function (req, res) {
+  db.getAllPosts(1) //change argument to req.body
+    .then((results) => {
+      let posts = results.rows;
+      db.getPostsLiked(1)
+        .then((likeResult) => {
+          let likedPosts = likeResult.rows.map(result => { return result.post_id });
+          posts.forEach(post => {
+            console.log('foreach')
+            likedPosts.includes(post.post_id) ? post.liked = false : post.liked = true;
+          })
+          res.json(posts);
+        })
+      // res.json(results.rows);
+    })
+    .catch((err) => {
+      console.log('feed had an error', err);
     })
 })
 
