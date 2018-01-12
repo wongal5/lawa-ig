df9d08745f7520f50344c0d299d5135cbc1b2111
Your Name <williamputnamjr@gmail.com>
2018-01-12 10:28:24 -0800

    fixed issues with modal db connection

--- client/src/components/prof_pg/ProfilePanel.jsx
+++ client/src/components/prof_pg/ProfilePanel.jsx
@@ -7,7 +7,7 @@ const ProfilePanel = (props) => (
   <div className='prof-panel-container'>
     <Grid centered>
       <Grid.Column width={4}>
-        <Image circular className='prof-avatar' src={props.user.avatar} />
+        <Image circular className='prof-avatar' src={props.user.prof_pic} />
       </Grid.Column>
       <Grid.Column width={9}>
         <Grid.Row className="first-panel-row">
@@ -25,20 +25,20 @@ const ProfilePanel = (props) => (
             <Modal size="mini" trigger={<span><span className='profile-sub-data-num'>{props.user.followers.length} </span>followers</span>}>
               <Modal.Header>Followers</Modal.Header>
               <Modal.Content scrolling><List divided verticalAlign='middle'>{props.user.followers.map((follower) => {
-                return <List.Item><span className="space"><List.Content className="follow">{follower}</List.Content></span><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
+                return <List.Item><Image avatar src={follower.prof_pic} /><List.Content className="follow">{follower.name}</List.Content><List.Content className="small-button" floated="right"><Button color="blue" size="mini">Following</Button></List.Content></List.Item>;
               })}</List></Modal.Content>
             </Modal></span>
           <span className='profile-sub-data'>
             <Modal size="mini" trigger={<span><span className='profile-sub-data-num'>{props.user.following.length} </span>followers</span>}>
               <Modal.Header>Following</Modal.Header>
               <Modal.Content scrolling><List divided verticalAlign='middle'>{props.user.following.map((following) => {
-                return <List.Item><List.Content className="follow">{following}</List.Content><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
+                return <List.Item><Image avatar src={following.prof_pic} /><List.Content className="follow">{following.name}</List.Content><List.Content className="small-button" floated="right"><Button color="blue" size="mini">Following</Button></List.Content></List.Item>;
               })}</List></Modal.Content>
             </Modal></span>
         </Grid.Row>
         <Grid.Row>
           <div className='header-caption'>
-            <span > {props.user.caption} </span>
+            <span > {props.user.description} </span>
           </div>
         </Grid.Row>
       </Grid.Column>
