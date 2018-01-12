be480fc563e6c725bed29f925e8eefba3a331a3f
Your Name <williamputnamjr@gmail.com>
2018-01-12 08:55:45 -0800

    about to do a pull request after rebase

--- client/src/components/prof_pg/ProfilePanel.jsx
+++ client/src/components/prof_pg/ProfilePanel.jsx
@@ -11,11 +11,7 @@ const ProfilePanel = (props) => (
       </Grid.Column>
       <Grid.Column width={9}>
         <Grid.Row className="first-panel-row">
-<<<<<<< HEAD
           <Header className='profile-name-title'>{props.user.name}</Header> 
-=======
-          <Header inline className='profile-name-title'>{props.user.username}</Header>
->>>>>>> trying to rebase
           {
             (props.isFollowed === true)
               ? <Button onClick={props.followUser} className='follow-button' basic>Following</Button>
@@ -29,14 +25,14 @@ const ProfilePanel = (props) => (
             <Modal size="mini" trigger={<span><span className='profile-sub-data-num'>{props.user.followers.length} </span>followers</span>}>
               <Modal.Header>Followers</Modal.Header>
               <Modal.Content scrolling><List divided verticalAlign='middle'>{props.user.followers.map((follower) => {
-                return <List.Item><span><List.Content className="follow">{follower}</List.Content></span><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
+                return <List.Item><span className="space"><List.Content className="follow">{follower}</List.Content></span><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
               })}</List></Modal.Content>
             </Modal></span>
           <span className='profile-sub-data'>
-            <Modal size="mini" trigger={<span><span className='profile-sub-data-num'>{props.user.following.length} </span>following</span>}>
+            <Modal size="mini" trigger={<span><span className='profile-sub-data-num'>{props.user.following.length} </span>followers</span>}>
               <Modal.Header>Following</Modal.Header>
               <Modal.Content scrolling><List divided verticalAlign='middle'>{props.user.following.map((following) => {
-                return <List.Item><List.Content className="follow">{following}</List.Content><br/><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
+                return <List.Item><List.Content className="follow">{following}</List.Content><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
               })}</List></Modal.Content>
             </Modal></span>
         </Grid.Row>
