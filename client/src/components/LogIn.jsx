ed551fdf4d8b3715b7cd1b643b95243216317775
Your Name <williamputnamjr@gmail.com>
2018-01-11 20:51:09 -0800

    altered log in front end to look nicer

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-// import config from '../../../server/config.js';
+import config from '../../../server/config.js';
+import { Button, Form, Grid, Header, Image, Icon, Message, Segment } from 'semantic-ui-react'
+
 
 
 
@@ -38,17 +40,64 @@ class LogIn extends React.Component {
 
 
   render() {
-
+    <style>{`
+        body > div,
+        body > div > div,
+        body > div > div > div.login-form {
+          height: 100%;
+        }
+    `}</style>
     return (
-      <div>
-        <a href={'https://www.facebook.com/v2.8/dialog/oauth?client_id=' + process.env.FB_ID + '&redirect_uri=http%3A%2F%2Flocalhost:3000%2Flogin%2Ffacebook%2Fcallback'}>Log in with Facebook</a>
-        <input name='email' ref={(input) => { this.email = input }} placeholder='email...'></input>
-        <input name='password' ref={(input) => { this.password = input }} placeholder='password...'></input>
-        <button>Sign Up</button>
-        <button onClick={(e) => {this.logOut(e)}}>Log Out</button>
+      // <div>
+      //   <a href={'https://www.facebook.com/v2.8/dialog/oauth?client_id=' + config.FACEBOOK_APP_ID + '&redirect_uri=http%3A%2F%2Flocalhost:3000%2Flogin%2Ffacebook%2Fcallback'}>Log in with Facebook</a>
+      //   <input name='email' ref={(input) => { this.email = input }} placeholder='Username or email'></input>
+      //   <input name='password' ref={(input) => { this.password = input }} placeholder='Password'></input>
+      //   <button>Sign Up</button>
+      //   <button onClick={(e) => {this.logOut(e)}}>Log Out</button>
+      // </div>
+      <div className='login-form'>
+        <Grid
+          textAlign='center'
+          style={{ height: '100%' }}
+          verticalAlign='middle'
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h1' color='instagram' textAlign='center'>
+              Instalawa
+        </Header>
+            <Button color="facebook"><Icon name="facebook" /><a href={'https://www.facebook.com/v2.8/dialog/oauth?client_id=' + config.FACEBOOK_APP_ID + '&redirect_uri=http%3A%2F%2Flocalhost:3000%2Flogin%2Ffacebook%2Fcallback'}>Log in with Facebook</a></Button>
+            <Form size='large'>
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  icon='user'
+                  iconPosition='left'
+                  placeholder='Username or email'
+                  ref={(input) => { this.email = input }}
+                />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                  ref={(input) => { this.password = input }}
+                />
+
+                <Button color='instagram' fluid size='large'>Login</Button>
+              </Segment>
+            </Form>
+            <Message>
+              New to us? <a href='/'>Sign Up</a>
+            </Message>
+          </Grid.Column>
+        </Grid>
       </div>
+
+
     );
   }
 }
 
 export default LogIn;
+
--- client/src/components/prof_pg/ProfilePanel.jsx
+++ client/src/components/prof_pg/ProfilePanel.jsx
@@ -29,14 +29,14 @@ const ProfilePanel = (props) => (
             <Modal size="mini" trigger={<span><span className='profile-sub-data-num'>{props.user.followers.length} </span>followers</span>}>
               <Modal.Header>Followers</Modal.Header>
               <Modal.Content scrolling><List divided verticalAlign='middle'>{props.user.followers.map((follower) => {
-                return <List.Item><span className="space"><List.Content className="follow">{follower}</List.Content></span><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
+                return <List.Item><span><List.Content className="follow">{follower}</List.Content></span><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
               })}</List></Modal.Content>
             </Modal></span>
           <span className='profile-sub-data'>
             <Modal size="mini" trigger={<span><span className='profile-sub-data-num'>{props.user.following.length} </span>followers</span>}>
               <Modal.Header>Following</Modal.Header>
               <Modal.Content scrolling><List divided verticalAlign='middle'>{props.user.following.map((following) => {
-                return <List.Item><List.Content className="follow">{following}</List.Content><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
+                return <List.Item><List.Content className="follow">{following}</List.Content><br/><List.Content className="small-button" floated="right"><Button color="blue" size="tiny">Following</Button></List.Content></List.Item>;
               })}</List></Modal.Content>
             </Modal></span>
         </Grid.Row>
