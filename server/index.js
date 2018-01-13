af767b2d9438d818f156f4bf89b5b007a6c95387
Albert Wong <a.wong9970@gmail.com>
2018-01-13 10:35:30 -0800

    Update index.js to allow access-control-allow-origin for testing purposes

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.options('/*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, Origin, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type, Authorization, Accept, Origin, Content-Length, X-Requested-With');
   res.header('Access-Control-Max-Age', 10);
   res.sendStatus(200);
 });
