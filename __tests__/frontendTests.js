8ec8fe490d1a61f9300f02d99916968194ef701c
wongal5 <a.wong9970@gmail.com>
2018-01-13 12:27:25 -0800

    updated with node fetch as node doesn't have native fetch support

--- __tests__/frontendTests.js
+++ __tests__/frontendTests.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from '../client/src/index.jsx';
 import renderer from 'react-test-renderer';
+import fetch from 'node-fetch';
 
 test('app renders correctly', () => {
   const tree = renderer
