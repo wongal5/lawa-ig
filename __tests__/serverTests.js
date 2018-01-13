e52921a5b1ecb7d4b7bd6f1d0e50b593ebf5e41c
wongal5 <a.wong9970@gmail.com>
2018-01-13 10:33:55 -0800

    removed db tests, will test through servers routes instead. added allow-origin header to help axios promises work

--- __tests__/serverTests.js
+++ __tests__/serverTests.js
@@ -4,17 +4,27 @@
  * https://facebook.github.io/jest/docs/en/expect.html
 */
 
-var axios = require('axios');
-var request = require('request');
+const axiosLib = require('axios');
+const request = require('request');
+const url = 'https://lawa-ig-staging.herokuapp.com/';
 
-// Opens connection to lawa_db database. This won't work in travisCI
+var axios = axiosLib.create({
+  baseURL: 'http://localhost:3000/',
+  timeout: 1000,
+  headers: { 'Access-Control-Allow-Origin': '*' }
+});
 
 describe('/profile endpoint', () => {
   test('to respond to get requests with profile user names', () => {
-    // using axios didn't work here, not sure why. Using request module with callbacks instead. 
-    // return expect(Promise.resolve(axios.get('http://localhost:3000/profile'))).resolves.toBeDefined();
-    request.get('http://localhost:3000/profile', function(err, response) {
-      expect(response.body[0]).toHaveProperty('name');
-    });
+    return axios.get('/profile')
+      .then(response => {
+        expect(response.data[0].label).toBe('Albert Wong');
+      })
+  });
+  test('to respond to get requests with profile user ids', () => {
+    return axios.get('/profile')
+      .then(response => {
+        expect(response.data[0].name).toBe(1);
+      })
   });
 });
